8948f0d6cd3ad20c122ffe7e77e2117dd9fa26d0
Vladislava96 <vladislava96.96@mail.ru>
2022-08-12 18:58:30 +0300

    init: start brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+  const OPEN_BRACKETS = ['(', '[', '{']
+  const BRACKETS_PAIR = {
+    [')']: '(',
+    [']']: '[',
+    ['}']: '{',
+  }
+
+  function isBracketsOk(str) {
+    let stack = [];
+
+    for (let i = 0; i < str.length; i++) {
+      let topElem = stack[stack.length - 1]
+      let currentSymbol = str[i];
+
+      if (str[i] === '|') {
+        if (stack.includes(str[i])) {
+          if (topElem === str[i]) {
+            stack.pop()
+          } else {
+            return false
+          }
+        } else {
+          stack.push(str[i])
+        }
+      }
+
+      if (OPEN_BRACKETS.includes(currentSymbol)) {
+        stack.push(currentSymbol)
+      } else {
+        if (!stack.length) {
+          return false
+        }
+        if (BRACKETS_PAIR[currentSymbol] === topElem) {
+          stack.pop()
+        } else {
+          return false
+        }
+      }
+    }
+    return stack.length === 0
+  }
+  return isBracketsOk(str)
 }
